d4e9b1157016e439f4e51fb31d2eed3bf6614d21
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-09-15 21:21:30 +0900

    Dev plan (#2)
    * Add base data
    
    * [WIP] Add memo
    
    * Add plan
    
    * Fix episode adding
    
    * [WIP] memo
    
    * Add memo
    
    * Update v1.1.0 [ci skip]

--- src/main.py
+++ src/main.py
@@ -19,45 +19,133 @@ from config import ASSET
 #
 #   1. Initialize
 #   2. Story memo
-#   3. Structure    - 1/8
+#   3. Structure    - 1/8: 3K
 #   4. Spec
-#   5. Plot         - 1/4
+#   5. Plot         - 1/4: 7K
 #   6. Scenes
-#   7. Conte        - 1/2
+#   7. Conte        - 1/2: 15K
 #   8. Layout
-#   9. Draft        - 1/1
+#   9. Draft        - 1/1: 30K
 #
 ################################################################
 
 # Constant
-TITLE = "作品タイトル"
-MAJOR, MINOR, MICRO = 0, 0, 1
-COPY = "コピィ"
-ONELINE = "一行説明"
-OUTLINE = "あらすじ"
-THEME = "テーマ"
-GENRE = "ジャンル"
-TARGET = "ターゲット（年代）"
-SIZE = "規定サイズ"
-CONTEST_INFO = "コンテスト情報"
-CAUTION = "注意事項"
-NOTE = "備考"
+TITLE = "拝啓、あなたが嫌いです"
+MAJOR, MINOR, MICRO = 1, 1, 0
+COPY = "あなたがずっと、嫌いでした"
+ONELINE = "仕事バカの夫は死後に犬型ロボットをプレゼントした。妻はそれに「ずっと嫌いでした」と告白する"
+OUTLINE = "約30000字の恋愛中編。仕事で忙しかった夫は死後に自分のAIを載せた犬型ロボットをプレゼントした。それに対して残された妻は告白する。ずっと嫌いでしたと"
+THEME = "人にとって言葉の定義は異なる"
+GENRE = "恋愛／SF"
+TARGET = "20-40years"
+SIZE = "原稿70-250枚（20K-80K）"
+CONTEST_INFO = "群像新人賞"
+CAUTION = ""
+NOTE = "エブリスタの妄想コンテストに応募した作品のリメイク"
 SITES = ["エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム"]
 TAGS = ["ドラマ",]
-RELEASED = (1, 1, 2020)
+RELEASED = (9, 1, 2020)
 
 
 # Episodes
-def ep_xxx(w: World):
-    return w.episode('episode_title',
-            outline="description")
+def ep_beginning(w: World):
+    return w.episode('書き出し',
+            )
 
+def ep_his_dead(w: World):
+    return w.episode("彼の死",
+            )
 
-def ch_main(w: World):
-    return w.chapter('main',
+def ep_dog_robo(w: World):
+    return w.episode("犬のロボット",
+            )
+
+def ep_truth(w: World):
+    return w.episode("真相",
             )
 
 
+# Chapters
+def ch_main(w: World):
+    return w.chapter('main',
+            ep_beginning(w),
+            ep_his_dead(w),
+            ep_dog_robo(w),
+            ep_truth(w),
+            )
+
+
+# Notes
+def write_note(w: World):
+    return w.writer_note("覚書",
+            )
+
+def plot_note(w: World):
+    return w.writer_note("プロットメモ",
+            "あなたが嫌いです、という告白文から始まる",
+            "嫌いというのが実は彼女にとっての「好き」だった、が真実",
+            "結婚した彼はその「嫌いが好き」を理解していて、嫌いと言われ続けていた",
+            "いつかは好きに変えたいと思っていたが、志半ばで倒れてしまう",
+            "彼女は死んだ彼に対して「嫌い」と日々を綴る",
+            "それはロボットに愚痴るようにして記録されていた",
+            "最後に全ての記録文章が「好き」に変わる",
+            "そこで娘も「好き」を知る",
+            "主人公の「過去」特に「家族からの扱われ方」について書いておく必要がある",
+            "それは直接的ではなく、想像できるように「家族」についてのピースをあちこちに散らばらせること",
+            )
+
+def chara_note(w: World):
+    return w.writer_note("人物メモ",
+            "主人公の彼女",
+            "隆文：主人公の夫で技術者",
+            "主人公の娘",
+            "ロボット犬",
+            "診療所の先生",
+            "ブライアン",
+            )
+
+def stage_note(w: World):
+    return w.writer_note("舞台メモ",
+            "住宅街、特に新興住宅地だった場所がいい",
+            "家庭、家族を思わせるところで、よく仲良さそうな家族が目に就く場所。公園が近いとか",
+            "でもその中にはごく稀にひどい家族も混ざっていると",
+            )
+
+def theme_note(w: World):
+    return w.writer_note("テーマメモ",
+            "人間は生きるために真実ですら歪める",
+            "人間の防衛本能として「嘘を真実にして」までも自分を肯定しようとする",
+            "本作では「嫌い」という言葉を吐き続けられた女性がそれを「好き」と変換して思い込むことで生きてこられた、という人生を抱えている",
+            "家族は正しいとか、家族だから愛があるとか、そんなまやかしはもう通用しない",
+            "家族であっても人間同士であり、そこには傷つけ合うことだってある",
+            "だからこそ、その中で生き延びる為に必死になって嘘を真実にする",
+            "虐待、それは何も暴力に限らない",
+            "言葉だって、いや言葉だからこそ人を傷つける",
+            "トラウマになる",
+            "これはそれが浄化されるまでの物語だ",
+            "最後まで読めばその「違和感」が感動に変わる",
+            "今までのものが上書きされ、リバーサル",
+            "○○ロス",
+            "大切なものを失った心の隙間を埋める「産業」としてのロボット",
+            "定義をする、ということと、その上書き",
+            "コンピュータの挙動",
+            "元勤め先はコンピュータ関係",
+            "なるべく対人でないものの方が良かった、という",
+            )
+
+def motif_note(w: World):
+    return w.writer_note("モチーフメモ",
+            "言葉の意味",
+            "人によって定義が異なる",
+            "小さい頃の虐待の記憶が意味を歪める",
+            "定義",
+            "虐待",
+            "自己防衛",
+            "ロスを癒やすロボットペット",
+            )
+
+
+# Main
 def main(): # pragma: no cover
     w = World.create_world(f"{TITLE}")
     w.config.set_version(MAJOR, MINOR, MICRO)
@@ -79,6 +167,12 @@ def main(): # pragma: no cover
     w.config.set_taginfos(*TAGS)
     w.config.set_released(*RELEASED)
     return w.run(
+            write_note(w),
+            plot_note(w),
+            chara_note(w),
+            stage_note(w),
+            theme_note(w),
+            motif_note(w),
             ch_main(w),
             )
 
