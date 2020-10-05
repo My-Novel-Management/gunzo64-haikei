6d53a95f072a6c02d2859aeba2f7575c94c1b1f8
nagisc007 <nagisc007@yahoo.co.jp>
2020-10-05 10:06:29 +0900

    [WIP] Add conte

--- src/scenes/Company.py
+++ src/scenes/Company.py
@@ -10,4 +10,11 @@ from storybuilder.builder.world import World
 
 
 ## scenes
-
+def his_company(w: World):
+    sato, ayano = w.get("satomi"), w.get("ayano")
+    return w.scene("彼の会社",
+            w.change_stage("Company"),
+            sato.come("メモの地図を見ながらきょろきょろと歩いている"),
+            ayano.be(),
+            sato.do(""),
+            )
